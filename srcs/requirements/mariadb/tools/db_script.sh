cd7dd3edfd757f14e4f6271c925534bbb6fc8023
Mary Kate Evangelidou <marykate_evan@outlook.com.gr>
2025-01-13 04:48:39 +0100

    after a million modifications i think now the script of MariaDB is ready. Added more control checks for the db initialization

---
+++ srcs/requirements/mariadb/tools/db_script.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# Checks if the database is already initialized
+if [ -d "/var/lib/mysql/mysql" ]; then
+	echo "Database already exists."
+else
+	echo "Initializing MariaDB database..."
+
+	# initializes the system database structure that MariaDB requires:
+	mysql_install_db --user=mysql --datadir=/var/lib/mysql
+
+	# Preparing the db_init.sql file:
+	echo "FLUSH PRIVILEGES;
+	CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;
+	CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+	GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%';
+	FLUSH PRIVILEGES;" > /tmp/db_init.sql
+
+	# runs MariaDB temporarily, in the background, without network connections:
+	mysqld_safe --skip-networking & 
+
+	# waits for MariaDB to be ready to accept connections
+	while ! mysqladmin ping --silent; do
+		echo "Waiting for MariaDB to be ready..."
+		sleep 1
+	done
+
+	# runs the SQL init script using the MariaDB client (mysql)
+	mysql < /tmp/db_init.sql
+
+	# shuts down the MariaDB server after initialization
+	mysqladmin shutdown
+
+	# clean-up of the used script:
+	rm -f /tmp/db_init.sql
+fi
+
+# Starts the properly initialized MariaDB server
+exec mysqld_safe --user=mysql
+
+echo "\033[0;32mMariaDB is ready for use!\033[0m"
