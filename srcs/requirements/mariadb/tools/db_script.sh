e42d02160d8e21d7cc4fb88b6f1ac068bfc8868e
Mary Kate Evangelidou <marykate_evan@outlook.com.gr>
2025-01-22 05:38:34 +0100

    the mariadb files and approach that seems to work for that container

--- srcs/requirements/mariadb/tools/db_script.sh
+++ srcs/requirements/mariadb/tools/db_script.sh
@@ -4,10 +4,10 @@ mkdir -p /var/lib/mysql /var/run/mysqld
 chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
 
 # Checks if the database is already initialized
-if [ "$(ls -A /var/lib/mysql/mysql)" ]; then
-    echo "Database directory is not empty; skipping initialization."
-else
-	echo "Initializing MariaDB database..."
+# if [ "$(ls -A /var/lib/mysql/mysql)" ]; then
+#     echo "Database directory is not empty; skipping initialization."
+# else
+# 	echo "Initializing MariaDB database..."
 # if [ -d "/var/lib/mysql/mysql" ]; then
 # 	echo "Database already exists."
 # else
@@ -17,11 +17,11 @@ else
 	# mysql_install_db --user=mysql --datadir=/var/lib/mysql
 
 	# Preparing the db_init.sql file:
-	echo "FLUSH PRIVILEGES;
-	CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;
-	CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
-	GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%';
-	FLUSH PRIVILEGES;" > /etc/mysql/db_init.sql
+echo "FLUSH PRIVILEGES;
+CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;
+CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%';
+FLUSH PRIVILEGES;" > /etc/mysql/db_init.sql
 
 	# runs MariaDB temporarily, in the background, without network connections:
 	# mysqld_safe --skip-networking & 
@@ -40,12 +40,18 @@ else
 
 	# clean-up of the used now, file we created above
 	# rm -f /tmp/db_init.sql
+# fi
+
+# Check if data base exists
+if [ -d "/var/lib/mysql/mysql" ]; then
+    echo "Database already exists."
+    # Use `mysqld_safe` only if the database is already initialized
+    mysqld_safe
+else
+    echo "Initializing new MariaDB database..."
+    mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+    mysqld
 fi
-
-echo "\033[0;32mMariaDB is ready for use!\033[0m"
-
-mysql_install_db
-mysqld
 
 # sleep 1000
 
